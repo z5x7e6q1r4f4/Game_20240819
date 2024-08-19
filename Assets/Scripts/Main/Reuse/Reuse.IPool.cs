8d61726d5e3ced3756caf58d9726dcc15e8e1c1f
Azai <asai.z5x7e6q1r4f4@gmail.com>
2024-08-20 00:19:02 +0900

    進度 : Reuse
    Reuse.Pool.IPool.IReuseable

---
+++ Assets/Scripts/Main/Reuse/Reuse.IPool.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Main
+{
+    partial class Reuse
+    {
+        public interface IPool<T> : IPool 
+            where T : class, IReuseable
+        {
+            object IPool.Get(bool onGet) => Get(onGet);
+            void IPool.Release(object item, bool onRelease) => Release((T)item, onRelease);
+            new T Get(bool onGet = true);
+            void Release(T item, bool onRelease = true);
+        }
+        public interface IPool : IDisposable
+        {
+            int AllCount { get; }
+            int ActiveCount { get; }
+            int InactiveCount { get; }
+            object Key { get; }
+            bool IsPrefab { get; }
+            object Get(bool onGet = true);
+            void Release(object item, bool onRelease = true);
+        }
+    }
+}
