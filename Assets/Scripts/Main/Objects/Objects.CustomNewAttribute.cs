723ab6583b539f92360c5175088cfe05dd92e828
Azai <asai.z5x7e6q1r4f4@gmail.com>
2024-08-19 23:28:32 +0900

    進度 : Object
    New.Clone,CustomNewAttribute.CustomCloneAttribute

---
+++ Assets/Scripts/Main/Objects/Objects.CustomNewAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Main
+{
+    public static partial class Objects
+    {
+        [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+        public abstract class CustomNewAttribute : Attribute
+        {
+            public abstract T New<T>(Type type, params object[] args);
+        }
+    }
+}
